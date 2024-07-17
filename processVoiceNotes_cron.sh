6b7a75fe6c7d6b2b661a5257f2c8ad05dbe47a22
george-m8 <geornb@gmail.com>
2024-07-17 10:20:35 +0000

    Shell script and Env fix

--- processVoiceNotes_cron.sh
+++ processVoiceNotes_cron.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
+LOGFILE=/home/george/VoiceNoteUploadToGSheetVER/cron_debug.log
+
 # Load environment contianing login credentials
-. VoiceNoteUploadToGSheetVER/sensetel_creds.env
+. /home/george/VoiceNoteUploadToGSheetVER/sensetel_creds.env
+
+# Check if environment variables are loaded
+if [ -z "$SENSETEL_USERNAME" ] || [ -z "$SENSETEL_PASSWORD" ]; then
+    echo "Error: Environment variables not loaded properly."
+    exit 1
+fi
 
 # Open relevant directory
-cd VoiceNoteUploadToGSheetVER
+cd /home/george/VoiceNoteUploadToGSheetVER || { echo "Error: Directory not found."; exit 1; }
+
+{
+    echo "Current directory:"
+    pwd
+} >> "$LOGFILE" 2>&1
 
 # Activate virtual environment
 source venv/bin/activate
 
 # Run script
-python uploadNewVoiceNotesBatch.py
+python uploadNewVoiceNotesBatch.py >> "$LOGFILE" 2>&1
 
-# Deactivate environment
-deactivate
-
-# Return to home directory
-cd
